ae098edd72f313109a1b5b0545f7064283ee1190
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-05-19 22:59:54 -0400

    edit file name

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+using System;
 
-namespace ClassesAndObjects
+namespace Bulldozer
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dozer = new BullDozer {"Yellow", "mini"};
+
+            var dozer = new BullDozer {"Red", "Large"};
+
+            Console.WriteLine("Bulldozer 1 is: " + dozer.color);
+            Console.WriteLine("Bulldozer 2 is: " + dozer2.color);
         }
+    }
+    public class BullDozer
+    {
+   
+    public BullDozer(string color, string size) 
+        {
+          this.color = color;
+              this.size = size;
+               
+        }
+            public string color { get; set; }
+            public string size { get; set; }
     }
 }
