db903426361ed89bfccd0c1ea9750455d5500c34
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-05-20 16:41:52 -0400

    Add encapsulation

---
+++ encapsulation.cs
@@ -0,0 +1,34 @@
+
+using System;
+
+namespace Bulldozer
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var worker = new FactoryWorker ();
+			var result = worker.AddNumbers(2, 3);
+
+			Console.WriteLine(result.ToString());
+            
+		}
+	}
+
+    public class FactoryWorker
+	{
+		public FactoryWorker () {
+			
+		}
+
+		public int AddNumbers (int num1, int num2) {
+			var result = num1 + num2;
+
+			result = result + 100;
+			result = result - 100;
+
+			return result;
+		}
+
+	}
+}
